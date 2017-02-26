cf958befedd9fb566ff338a81de31fc3dc23e38b
Ann Dai <gofarann@gmail.com>
2017-02-26 13:32:48 -0800

    resintalled minitest reporter

--- 02-alouette/alouette_spec.rb
+++ 02-alouette/alouette_spec.rb
@@ -1,6 +1,8 @@
 gem 'minitest', '>= 5.0.0'
 require 'minitest/reporters'
 require 'minitest/autorun'
+# require 'minitest/skip_dsl'
+
 require_relative 'alouette'
 
 Minitest::Reporters.use!
@@ -18,12 +20,12 @@ describe Alouette do
     end
 
     it "generates the correct line for the first verse" do
-      skip
+
       Alouette.lines_for_verse(0).must_equal ['Et la tête!']
     end
 
     it "generates the correct lines for the third verse" do
-      skip
+
       expected_lines = [
         "Et les yeux!",
         "Et le bec!",

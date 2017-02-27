9c7fa2f8e04543e07acf036e08390f9d5d57fac1
Ann Dai <gofarann@gmail.com>
2017-02-27 09:14:39 -0800

    deleted new line after ahhh phrase

--- 02-alouette/alouette.rb
+++ 02-alouette/alouette.rb
@@ -20,61 +20,61 @@ class Alouette
 
 
 
-        def lines_for_verse(verse_number)
-          @@lines_for_verse = []
+      def lines_for_verse(verse_number)
+        @@lines_for_verse = []
 
-          index = verse_number
-          loop_number = verse_number + 1
+        index = verse_number
+        loop_number = verse_number + 1
 
-          loop_number.times do
-            @@lines_for_verse << "Et "+ "#{@@la_snippet[index]}"
-            index -= 1
-          end
-
-          return @@lines_for_verse
-
+        loop_number.times do
+          @@lines_for_verse << "Et "+ "#{@@la_snippet[index]}"
+          index -= 1
         end
 
-        def verse(verse_number)
+        return @@lines_for_verse
 
-          verse = []
-
-          verse << ("Je te plumerai" + " " + "#{@@la_snippet[verse_number]}")
-          verse << ("Je te plumerai" + " " + "#{@@la_snippet[verse_number]}")
-
-
-           Alouette.lines_for_verse(verse_number).each do |phrase|
-            verse << "#{phrase}"
-            verse << "#{phrase}"
-
-          end
-
-          verse <<  "Alouette!\n" +
-          "Alouette!\n" +
-          "A-a-a-ah\n"
-
-
-return verse.join("\n")
-        end
-
-        def sing
-          verse_number = 0
-
-          8.times do
-            puts "Alouette, gentille alouette,\n" +
-            "Alouette, je te plumerai." +
-            "\n"
-
-            puts Alouette.verse(verse_number)
-            verse_number += 1
-          end
-          puts "Alouette, gentille alouette,\n"+
-          "Alouette, je te plumerai.\n"
-        end
       end
 
+      def verse(verse_number)
+
+        verse = []
+
+        verse << ("Je te plumerai" + " " + "#{@@la_snippet[verse_number]}").delete!('!') + "."
+        verse << ("Je te plumerai" + " " + "#{@@la_snippet[verse_number]}").delete!('!') + "."
+
+
+        Alouette.lines_for_verse(verse_number).each do |phrase|
+          verse << "#{phrase}"
+          verse << "#{phrase}"
+
+        end
+
+        verse <<  "Alouette!\n" +
+        "Alouette!\n" +
+        "A-a-a-ah"
+
+
+        return verse.join("\n")
+      end
+
+      def sing
+        verse_number = 0
+
+        8.times do
+          puts "Alouette, gentille alouette,\n" +
+          "Alouette, je te plumerai." +
+          "\n"
+
+          puts Alouette.verse(verse_number)
+          verse_number += 1
+        end
+        puts "Alouette, gentille alouette,\n"+
+        "Alouette, je te plumerai.\n"
+      end
     end
 
-    # Alouette.sing.class
-    puts Alouette.verse(2)
-    # puts Alouette.sing
+  end
+
+  # Alouette.sing.class
+  puts Alouette.verse(2)
+  # puts Alouette.sing
